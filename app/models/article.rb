7291ffe0df9eeba7014eae0e7c006cc23c56a9d6
Brett Peters <brett.peters1@gmail.com>
2015-12-04 18:26:38 +0000

    tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,18 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
   validates :title, :body, presence: true
+  
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+  
+  def tag_list=(tags_string)
+    tags = tags_string.split(",").collect { |t| t.downcase.strip }.uniq
+    new_or_found_tags = tags.collect { |t| Tag.find_or_create_by(name: t) }
+    self.tags = new_or_found_tags
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  has_many :taggings
   validates :author_name, :body, presence: true
 end
